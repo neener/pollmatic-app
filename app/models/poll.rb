8957c4433ad566f0cd730adb74eaf76a5529eee2
neener <janinemrosen@gmail.com>
2017-02-28 03:23:52 -0500

    updating spec

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ApplicationRecord
 	belongs_to :user
 	has_many :poll_options
 	has_many :votes, through: :poll_options
-	
+	validates_presence_of :user_id, :question
 
 	def poll_option_options=(options)
 		options.reject(&:blank?).each do |option|
