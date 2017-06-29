2ec64ccb1c559d0cb5179a91da99abfdbf727173
neener <janinemrosen@gmail.com>
2017-06-29 17:33:59 -0400

    creating an empty div for rendered page content

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$("a.load_active_polls").on("click", function(e){
 		$.ajax('/polls.json').done(function(data){
-			$("body").html("")
+			$("div.content").html("")
 			data.forEach(function(poll){
 				var link = '<a href="/polls/' + poll.id + '">' + poll.question + '</a>' + '</br>'
-				$("body").append(link)
+				$("div.content").append(link)
 			})
 		})
 		e.preventDefault();
@@ -12,10 +12,10 @@ $(document).ready(function(){
 
 	$("a.load_expired_polls").on("click", function(e){
 		$.ajax('/polls/expired.json').done(function(data){
-			$("body").html("")
+			$("div.content").html("")
 			data.forEach(function(poll){
 				var link = '<a href="/polls/' + poll.id + '">' + poll.question + '</a>' + '</br>'
-				$("body").append(link)
+				$("div.content").append(link)
 			})
 		})
 		e.preventDefault();
